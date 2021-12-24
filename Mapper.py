3e0f1e5f0f715c8455857af5944ed4112e629e7f
Akh4th <68875577+Akh4th@users.noreply.github.com>
2021-12-24 19:21:03 +0200

    Update Mapper.py

--- Mapper.py
+++ Mapper.py
@@ -1,11 +1,13 @@
 from socket import *
+import socket as socket1
 import time
+from termcolor import colored as c
 import ipaddress
 global record
 
-# Determine weather to create a log file or not 
-record = input("Would you like to record the scanning ?\n[Yes/No] : ")
-if record.upper() == "YES":
+# Determine weather to create a log file or not
+record = input("Would you like to record the scanning ?" + c("\n[Yes/No] : ", "red"))
+if record.upper() == "YES" or record == 1:
     record = True
 else:
     record = False
@@ -25,46 +27,67 @@ def rec():
     file.close()
 
 
-while True:
+def ver(host, ser):
+    s = socket1.socket()
+    s.connect((host, ser))
+    if ser == 80:
+        x = "GET / HTTP/1.0\r\n\r\n"
+        s.send(x.encode())
+        print(c(f"http://{ip}/", "blue") + " - " + s.recv(1024).decode().split("\n")[1])
+    elif ser == 443:
+        print(c(f"https://{ip}/", "blue"))
+    else:
+        print(s.recv(1024).decode())
+
+
+time.sleep(1)
+res = {}
+try:
+    ip = input("\nEnter the " + c("IP Address", "red") + " you'd like to scan : ")
+    # Getting only legit values
+    try:
+        print("\nPlease enter ports range")
+        portA = int(input(c("Starts with : ", "blue")))
+        portB = int(input(c("Ends with : ", "yellow")))
+        while portA < 0:
+            portA = input(c("Port number has to be positive number.", "green"))
+        while portB < 0:
+            portB = input(c("Port number has to be positive number.", "green"))
+    except ValueError:
+        print(c("Port has to be a number !", "red"))
+        quit()
+    # Validating IP Address for scanning
     try:
         tim = time.time()
-        print("Hello user, type 'done' whenever you want to quit the program")
-        ip = input("Enter the IP Address you'd like to scan : ")
-        print("Please enter ports range\n")
-        # Getting only legit values
-        try:
-            portA = int(input("Starts with : "))
-            portB = int(input("Ends with : "))
-            while portA < 0:
-                portA = input("Port number has to be positive number.")
-            while portB < 0:
-                portB = input("Port number has to be positive number.")
-        except ValueError:
-            print("Port has to be a number !")
-            continue
-        # Validating IP Address for scanning
         if ipaddress.ip_address(ip):
-            try:
-                if record:
-                    rec()
-                print(f"Scanning the target {ip} in range of {portA}-{portB}\n")
-                for i in range(portA, portB):
-                    soc = socket(AF_INET, SOCK_STREAM)
-                    con = soc.connect_ex((ip, i))
-                    if (con == 0):
-                        print(f"Port number {i} is opened !")
-                        if record:
-                            rec2(i)
-                    soc.close()
-            except ipaddress.AddressValueError:
-                print("The IP Address is not responding.\nPlease try again.")
-                
-        # Two scenarios when the users wants to quit.
-        elif ip.upper() == 'DONE':
-            print("Thank you for using.\nCya next time.")
-            break
-    except KeyboardInterrupt:
-        print("Keyboard interrupt detected !\nABORTING !!!")
-        break
-
+            if record:
+                rec()
+            print("\nScanning the target " + c(ip, "red") + " in range of " + c(str(portA) + "-" + str(portB) + "\n",
+                                                                              "blue"))
+            print(c("PORTS\t\t", "green") + c("PROCESS\t\t", "blue") + c("DURATION\t\t", "yellow"))
+            for i in range(portA, portB + 1):
+                amount = portB - portA + 1
+                per = (int(i) * 100) / int(amount)
+                long = time.time() - tim
+                soc = socket(AF_INET, SOCK_STREAM)
+                con = soc.connect_ex((ip, i))
+                if con == 0:
+                    res[i] = socket1.getservbyport(i, "tcp")
+                    if record:
+                        rec2(i + " - " + socket1.getservbyport(i, "tcp"))
+                print("\r" + c(str(i) + "/" + str(amount), "green") + "\t\t" + c(str(per) + "%\t\t", "blue") + c(
+                    str(format(round(long, 2)) + "s\t\t", ), "yellow"), end=" ")
+                soc.close()
+            res1 = res.items()
+            print(c("\n\nSCAN IS OVER, ", "red") + c(len(res), "yellow") + c(" PORTS WERE FOUND !\n", "red"))
+            time.sleep(1)
+            for item in res1:
+                port, serv = item
+                print("The service " + c(serv, "red") + " is active, port : " + c(port, "red"))
+                ver(ip, port)
+    except ValueError:
+        print(c("The IP Address is not responding.\nPlease try again.\n", "red"))
+except KeyboardInterrupt:
+    print("Keyboard interrupt detected !\nABORTING !!!")
+    quit()
 
