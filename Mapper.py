9991a68af19057b2bba412d0b3c43f2592e46816
Akh4th <68875577+Akh4th@users.noreply.github.com>
2022-01-09 12:50:10 +0200

    Update Mapper.py

--- Mapper.py
+++ Mapper.py
@@ -1,23 +1,41 @@
-import argparse
-import socket
 import socket as s
 from socket import *
-import ipaddress
+import ipaddress, os, argparse
 
 ports = []
 
 # Arguments Parser
-p = argparse.ArgumentParser(description="Python Port Scanner by Akh4th !", usage="Separate hosts by ',' or '-' in case of ip range")
+p = argparse.ArgumentParser(description="Python Port Scanner by Akh4th !", epilog="Host specification : ['ip', 'ip1-3', 'ip1 , ip2']", usage="Mapper.py {host specification} {--port/s specification} [-O output_file] [-v] [-Gv] [-Ch]")
 p.add_argument("host", help="IP Address to search", nargs='*', type=str)
-p.add_argument("-Gv", "--get_version", action="store_true", help="Get service's version")
 p.add_argument("-O", "--Output", help="Prints output into a file", metavar="")
 p.add_argument("-v", "--verbose", help="Prints process verbosely", action="store_true")
+p.add_argument("-Gv", "--get_version", action="store_true", help="Get service's version")
+p.add_argument("-Ch", "--check_host", help="Ping host to check if a alive", action="store_true")
 
 g = p.add_mutually_exclusive_group()
 g.add_argument("-P", "--ports", type=int, help="Ports number range, default is 1000.", default=1000, metavar="")
 g.add_argument("-p", "--port", type=int, help="Specific port number.", metavar="")
 
 args = p.parse_args()
+
+
+# Check if IP is alive
+def is_up(IP):
+    if args.verbose:
+        print(f"Checking if {IP} is alive...")
+        if os.system(f"ping -c 1 {IP} > /dev/null") == 0:
+            print("Host is up !")
+            return True
+        else:
+            print("Host is down !")
+            return False
+    else:
+        if os.system(f"ping -c 1 {IP} > /dev/null") == 0:
+            print("Host is up !")
+            return True
+        else:
+            print("Host is down !")
+            return False
 
 
 # Scanning multiply ports
@@ -57,11 +75,11 @@ def scan(port, IP):
         if args.get_version:
             if args.Output:
                 with open(args.Output, "w+") as file:
-                    file.write(f"Port number {port} is ON ! \nService Version : {ver(service=args.port, IP=args.host)}")
+                    file.write(f"Port number {port} is ON ! \nService Version : {ver(service=args.port, IP=args.host[0])}")
                     file.close()
-                return f"Port number {port} is ON ! \nService Version : {ver(service=args.port, IP=args.host)}"
+                return f"Port number {port} is ON ! \nService Version : {ver(service=args.port, IP=args.host[0])}"
             else:
-                return f"Port number {port} is ON ! \nService Version : {ver(service=args.port, IP=args.host)}"
+                return f"Port number {port} is ON ! \nService Version : {ver(service=args.port, IP=args.host[0])}"
         else:
             if args.Output:
                 with open(args.Output, "w+") as file:
@@ -93,11 +111,20 @@ if __name__ == "__main__":
         if len(args.host) == 1:
             try:
                 ipaddress.IPv4Address(args.host[0])
+                if args.check_host:
+                    if not is_up(args.host[0]):
+                        quit()
                 # Single Port
                 if args.port:
                     print(scan(port=args.port, IP=str(args.host[0])))
                 # Ports range
+                elif args.ports:
+                    scans(port=args.ports, IP=str(args.host[0]))
+                    for i in ports:
+                        print(i)
                 else:
+                    if args.verbose:
+                        print("No ports range was mentioned, trying first 1000 ports.")
                     scans(port=args.ports, IP=str(args.host[0]))
                     for i in ports:
                         print(i)
@@ -108,6 +135,9 @@ if __name__ == "__main__":
                         ip = args.host[0].split(".")[0] + "." + args.host[0].split(".")[1] + "." + \
                              args.host[0].split(".")[
                                  2] + "." + str(address)
+                        if args.check_host:
+                            if not is_up(ip):
+                                quit()
                         if ipaddress.IPv4Address(ip):
                             if args.port:
                                 print(scan(port=args.port, IP=ip))
@@ -119,6 +149,11 @@ if __name__ == "__main__":
                             raise ipaddress.AddressValueError
         elif "," in args.host:
             for ip in args.host:
+                if "," in ip and len(ip) > 1:
+                    ip = ip.split(',')[0]
+                if args.check_host and ip != ",":
+                    if not is_up(ip):
+                        quit()
                 if ip == ",":
                     continue
                 elif ipaddress.IPv4Address(ip):
@@ -130,6 +165,6 @@ if __name__ == "__main__":
                             print(i)
                 else:
                     raise ipaddress.AddressValueError
-    # Keyboard interrupt 
+    # Keyboard interrupt !
     except KeyboardInterrupt:
         print("Take care !")
